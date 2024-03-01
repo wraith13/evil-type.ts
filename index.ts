d015950c18821218f6bd59ff06ac2a8c5d8c7391
道化師 <wraith13@hotmail.co.jp>
2024-03-02 00:55:05 +0900

    🛠 実装 ( WIP )

--- index.ts
+++ index.ts
@@ -1,3 +1,20 @@
-export module evilType
+'use strict';
+const schema = "https://raw.githubusercontent.com/wraith13/evil-type.ts/master/resource/type-schema.json#";
+const startAt = new Date();
+const getBuildTime = () => new Date().getTime() - startAt.getTime();
+const jsonPath = process.argv[2];
+const mode = process.argv[3] || "default";
+console.log(`🚀 ${jsonPath} ${mode} build start: ${startAt}`);
+try
 {
+    const fs = require("fs");
+    const makePath = (...path : (undefined | string)[]) => path.map(i => undefined !== i ? i: "").join("").replace(/\/\.\//gm, "/");
+    const fget = (path: string) => fs.readFileSync(path, { encoding: "utf-8" });
+    console.log(`✅ ${jsonPath} ${mode} build end: ${new Date()} ( ${(getBuildTime() / 1000).toLocaleString()}s )`);
 }
+catch(error)
+{
+    console.error(error);
+    console.log(`🚫 ${jsonPath} ${mode} build failed: ${new Date()} ( ${(getBuildTime() / 1000).toLocaleString()}s )`);
+}
+// how to run: `node ./index.js .......`
