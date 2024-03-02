d7f3dd5c403e72dddb23c990e69aa87d78cd10cb
道化師 <wraith13@hotmail.co.jp>
2024-03-02 18:06:50 +0900

    🛠 実装 ( WIP )

--- index.ts
+++ index.ts
@@ -3,18 +3,17 @@ const schema = "https://raw.githubusercontent.com/wraith13/evil-type.ts/master/r
 const startAt = new Date();
 const getBuildTime = () => new Date().getTime() - startAt.getTime();
 const jsonPath = process.argv[2];
-const mode = process.argv[3] || "default";
-console.log(`🚀 ${jsonPath} ${mode} build start: ${startAt}`);
+console.log(`🚀 ${jsonPath} build start: ${startAt}`);
 try
 {
     const fs = require("fs");
-    const makePath = (...path : (undefined | string)[]) => path.map(i => undefined !== i ? i: "").join("").replace(/\/\.\//gm, "/");
     const fget = (path: string) => fs.readFileSync(path, { encoding: "utf-8" });
-    console.log(`✅ ${jsonPath} ${mode} build end: ${new Date()} ( ${(getBuildTime() / 1000).toLocaleString()}s )`);
+    console.log(`✅ ${jsonPath} build end: ${new Date()} ( ${(getBuildTime() / 1000).toLocaleString()}s )`);
+    const typeSource = fget(jsonPath);
 }
 catch(error)
 {
     console.error(error);
-    console.log(`🚫 ${jsonPath} ${mode} build failed: ${new Date()} ( ${(getBuildTime() / 1000).toLocaleString()}s )`);
+    console.log(`🚫 ${jsonPath} build failed: ${new Date()} ( ${(getBuildTime() / 1000).toLocaleString()}s )`);
 }
 // how to run: `node ./index.js .......`
