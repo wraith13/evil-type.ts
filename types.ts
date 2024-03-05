4f667a6ba422f8aaae209460aa4ea4d51543d1bd
道化師 <wraith13@hotmail.co.jp>
2024-03-05 15:58:24 +0900

    🛠 実装 ( WIP )

--- types.ts
+++ types.ts
@@ -4,27 +4,33 @@ export module Types
     export interface TypeSchema
     {
         $ref: "https://raw.githubusercontent.com/wraith13/evil-type.ts/master/resource/type-schema.json#";
-        defines: { [key: string]: Define };
+        defines: { [key: string]: Define; };
     }
     export interface BaseDefine
     {
-        type: Define["type"];
+        $type: $type;
     }
     export interface ModuleDefine extends BaseDefine
     {
-        type: "module";
+        $type: "module";
+        members: { [key: string]: Define; };
     }
-    export interface ValueDefine extends BaseDefine
+    export interface ValueDefine<ValueType> extends BaseDefine
     {
-        type: "value";
+        $type: "value";
+        value: ValueType;
     }
     export interface TypeDefine extends BaseDefine
     {
-        type: "type";
+        $type: "type";
+        define: TypeOfInterface;
     }
     export interface InterfaceDefine extends BaseDefine
     {
-        type: "interface";
+        $type: "interface";
+        members: { [key: string]: TypeOfInterface; };
     }
-    export type Define = ModuleDefine | ValueDefine | TypeDefine | InterfaceDefine;
+    export type $type = Define["$type"];
+    export type TypeOfInterface = TypeDefine | InterfaceDefine;
+    export type Define = ModuleDefine | ValueDefine<any> | TypeOfInterface;
 }
