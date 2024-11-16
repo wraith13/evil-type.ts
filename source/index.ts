810ba89cd47f53a414aab8153b4563b2f20a5be3
道化師 <645907+wraith13@users.noreply.github.com>
2024-11-16 18:31:38 +0900

    🛠 実装 ( WIP )

--- source/index.ts
+++ source/index.ts
@@ -1183,32 +1183,24 @@ export namespace Build
                     }
                     if
                     (
-                        (undefined === aTarget.value.minimum || aTarget.value.minimum <= (bTarget.value.minimum ?? aTarget.value.minimum)) &&
-                        (undefined === aTarget.value.maximum || (bTarget.value.maximum ?? aTarget.value.maximum) <= aTarget.value.maximum) &&
-                        (undefined === aTarget.value.exclusiveMinimum || aTarget.value.exclusiveMinimum <= (bTarget.value.exclusiveMinimum ?? aTarget.value.exclusiveMinimum)) &&
-                        (undefined === aTarget.value.exclusiveMaximum || (bTarget.value.exclusiveMaximum ?? aTarget.value.exclusiveMaximum) <= aTarget.value.exclusiveMaximum) &&
-                        (undefined === aTarget.value.minimum || undefined === bTarget.value.exclusiveMinimum || aTarget.value.minimum < bTarget.value.exclusiveMinimum) &&
-                        (undefined === aTarget.value.maximum || undefined === bTarget.value.exclusiveMaximum || bTarget.value.exclusiveMaximum < aTarget.value.maximum) &&
-                        (undefined === aTarget.value.exclusiveMinimum || undefined === bTarget.value.minimum || aTarget.value.exclusiveMinimum < bTarget.value.minimum) &&
-                        (undefined === aTarget.value.exclusiveMaximum || undefined === bTarget.value.maximum || bTarget.value.maximum < aTarget.value.exclusiveMaximum) &&
-                        (undefined === aTarget.value.multipleOf || Number.isInteger((bTarget.value.multipleOf ?? aTarget.value.multipleOf) /aTarget.value.multipleOf)) &&
-                        (true !== aTarget.value.safeNumber || true === bTarget.value.safeNumber)
+                        (undefined === aTarget.value.minLength || aTarget.value.minLength <= (bTarget.value.minLength ?? aTarget.value.minLength)) &&
+                        (undefined === aTarget.value.maxLength || (bTarget.value.maxLength ?? aTarget.value.maxLength) <= aTarget.value.maxLength) &&
+                        (undefined === (<Type.FormatStringType>aTarget.value).format || (<Type.FormatStringType>aTarget.value).format === ((<Type.FormatStringType>bTarget.value).format ?? (<Type.FormatStringType>aTarget.value).format)) &&
+                        (undefined === (<Type.PatternStringType>aTarget.value).pattern || (<Type.PatternStringType>aTarget.value).pattern === ((<Type.PatternStringType>bTarget.value).pattern ?? (<Type.PatternStringType>aTarget.value).pattern)) &&
+                        (undefined === (<Type.FormatStringType>aTarget.value).format || (<Type.FormatStringType>aTarget.value).format === ((<Type.FormatStringType>bTarget.value).format ?? (<Type.FormatStringType>aTarget.value).format)) &&
+                        (<Type.FormatStringType & Type.PatternStringType>aTarget.value).regexpFlags === (<Type.FormatStringType & Type.PatternStringType>bTarget.value).regexpFlags
                     )
                     {
                         return "wide";
                     }
                     if
                     (
-                        (undefined === bTarget.value.minimum || bTarget.value.minimum <= (aTarget.value.minimum ?? bTarget.value.minimum)) &&
-                        (undefined === bTarget.value.maximum || (aTarget.value.maximum ?? bTarget.value.maximum) <= bTarget.value.maximum) &&
-                        (undefined === bTarget.value.exclusiveMinimum || bTarget.value.exclusiveMinimum <= (aTarget.value.exclusiveMinimum ?? bTarget.value.exclusiveMinimum)) &&
-                        (undefined === bTarget.value.exclusiveMaximum || (aTarget.value.exclusiveMaximum ?? bTarget.value.exclusiveMaximum) <= bTarget.value.exclusiveMaximum) &&
-                        (undefined === bTarget.value.minimum || undefined === aTarget.value.exclusiveMinimum || bTarget.value.minimum < aTarget.value.exclusiveMinimum) &&
-                        (undefined === bTarget.value.maximum || undefined === aTarget.value.exclusiveMaximum || aTarget.value.exclusiveMaximum < bTarget.value.maximum) &&
-                        (undefined === bTarget.value.exclusiveMinimum || undefined === aTarget.value.minimum || bTarget.value.exclusiveMinimum < aTarget.value.minimum) &&
-                        (undefined === bTarget.value.exclusiveMaximum || undefined === aTarget.value.maximum || aTarget.value.maximum < bTarget.value.exclusiveMaximum) &&
-                        (undefined === bTarget.value.multipleOf || Number.isInteger((aTarget.value.multipleOf ?? bTarget.value.multipleOf) /bTarget.value.multipleOf)) &&
-                        (true !== bTarget.value.safeNumber || true === aTarget.value.safeNumber)
+                        (undefined === bTarget.value.minLength || bTarget.value.minLength <= (aTarget.value.minLength ?? bTarget.value.minLength)) &&
+                        (undefined === bTarget.value.maxLength || (aTarget.value.maxLength ?? bTarget.value.maxLength) <= bTarget.value.maxLength) &&
+                        (undefined === (<Type.FormatStringType>bTarget.value).format || (<Type.FormatStringType>bTarget.value).format === ((<Type.FormatStringType>aTarget.value).format ?? (<Type.FormatStringType>bTarget.value).format)) &&
+                        (undefined === (<Type.PatternStringType>bTarget.value).pattern || (<Type.PatternStringType>bTarget.value).pattern === ((<Type.PatternStringType>aTarget.value).pattern ?? (<Type.PatternStringType>bTarget.value).pattern)) &&
+                        (undefined === (<Type.FormatStringType>bTarget.value).format || (<Type.FormatStringType>bTarget.value).format === ((<Type.FormatStringType>aTarget.value).format ?? (<Type.FormatStringType>bTarget.value).format)) &&
+                        (<Type.FormatStringType & Type.PatternStringType>bTarget.value).regexpFlags === (<Type.FormatStringType & Type.PatternStringType>aTarget.value).regexpFlags
                     )
                     {
                         return "narrow";
