97af790810cbafe105376d00888377ae65ac70a0
道化師 <645907+wraith13@users.noreply.github.com>
2024-11-19 19:06:01 +0900

    🛠 実装 ( WIP )

--- source/index.ts
+++ source/index.ts
@@ -893,7 +893,7 @@ export namespace Build
                         (undefined === aTarget.value.maximum || undefined === bTarget.value.exclusiveMaximum || bTarget.value.exclusiveMaximum < aTarget.value.maximum) &&
                         (undefined === aTarget.value.exclusiveMinimum || undefined === bTarget.value.minimum || aTarget.value.exclusiveMinimum < bTarget.value.minimum) &&
                         (undefined === aTarget.value.exclusiveMaximum || undefined === bTarget.value.maximum || bTarget.value.maximum < aTarget.value.exclusiveMaximum) &&
-                        (undefined === aTarget.value.multipleOf || Number.isInteger((bTarget.value.multipleOf ?? aTarget.value.multipleOf) /aTarget.value.multipleOf)) &&
+                        (undefined === aTarget.value.multipleOf || (undefined !== bTarget.value.multipleOf && Number.isInteger(bTarget.value.multipleOf /aTarget.value.multipleOf))) &&
                         (true !== aTarget.value.safeInteger || true === bTarget.value.safeInteger)
                     )
                     {
